93cee05bc3f694c816d184ffda6280061ec244cf
nastya <ntya034@gmail.com>
2024-12-08 12:34:54 +0300

    Добавьте файлы проекта.

---
+++ Try4/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Try4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.dtpRegistrationDate = new System.Windows.Forms.DateTimePicker();
+            this.chkHasTeam = new System.Windows.Forms.CheckBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(80, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Имя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(80, 600);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(611, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Подтверждение ознакомления правил игры";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(80, 500);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(275, 37);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Дата регистрации";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(80, 400);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 37);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Возраст";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(80, 300);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(248, 37);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Номер телефона";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(80, 200);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(140, 37);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Фамилия";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtFirstName.Location = new System.Drawing.Point(400, 100);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(400, 44);
+            this.txtFirstName.TabIndex = 6;
+            this.txtFirstName.Text = "Введите имя";
+            this.txtFirstName.TextChanged += new System.EventHandler(this.txtFirstName_TextChanged);
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtLastName.Location = new System.Drawing.Point(400, 200);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(400, 44);
+            this.txtLastName.TabIndex = 7;
+            this.txtLastName.Text = "Введите фамилию";
+            // 
+            // txtAge
+            // 
+            this.txtAge.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtAge.Location = new System.Drawing.Point(400, 400);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(400, 44);
+            this.txtAge.TabIndex = 8;
+            this.txtAge.Text = "Не менее 16 лет";
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtPhoneNumber.Location = new System.Drawing.Point(400, 300);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(400, 44);
+            this.txtPhoneNumber.TabIndex = 9;
+            this.txtPhoneNumber.Text = "+7(xxx)xxx-xx-xx";
+            // 
+            // dtpRegistrationDate
+            // 
+            this.dtpRegistrationDate.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dtpRegistrationDate.Location = new System.Drawing.Point(400, 500);
+            this.dtpRegistrationDate.Name = "dtpRegistrationDate";
+            this.dtpRegistrationDate.Size = new System.Drawing.Size(400, 44);
+            this.dtpRegistrationDate.TabIndex = 10;
+            // 
+            // chkHasTeam
+            // 
+            this.chkHasTeam.AutoSize = true;
+            this.chkHasTeam.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.chkHasTeam.Location = new System.Drawing.Point(800, 600);
+            this.chkHasTeam.Name = "chkHasTeam";
+            this.chkHasTeam.Size = new System.Drawing.Size(234, 41);
+            this.chkHasTeam.TabIndex = 11;
+            this.chkHasTeam.Text = "Ознакомлен(а)";
+            this.chkHasTeam.UseVisualStyleBackColor = true;
+            this.chkHasTeam.CheckedChanged += new System.EventHandler(this.chkHasTeam_CheckedChanged);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSave.Location = new System.Drawing.Point(500, 700);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(300, 100);
+            this.btnSave.TabIndex = 12;
+            this.btnSave.Text = "Регистрация";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 27.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(117, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(1000, 49);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Регистрация на страйкбольную игру";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 921);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.chkHasTeam);
+            this.Controls.Add(this.dtpRegistrationDate);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.DateTimePicker dtpRegistrationDate;
+        private System.Windows.Forms.CheckBox chkHasTeam;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Try4/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Try4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+            private void btnSave_Click(object sender, EventArgs e)
+        {
+            string firstName = txtFirstName.Text;
+            string lastName = txtLastName.Text;
+            string phoneNumber = txtPhoneNumber.Text;
+            string age = txtAge.Text;
+            string registrationDate = dtpRegistrationDate.Value.ToString("yyyy-MM-dd");
+            bool hasTeam = chkHasTeam.Checked;
+
+            string registrationData = $"{firstName};{lastName};{phoneNumber};{age};{registrationDate};{hasTeam}";
+
+            // Сохранение данных в текстовый файл
+            using (StreamWriter writer = new StreamWriter("registrations.txt", true))
+            {
+                writer.WriteLine(registrationData);
+            }
+
+            MessageBox.Show("Данные успешно сохранены!");
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkHasTeam_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtFirstName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
